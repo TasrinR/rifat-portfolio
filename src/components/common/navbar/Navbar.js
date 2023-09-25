aac064b0edb3ede12e154b85b8e784001ab629a0
tasrinr <kanijtasrin090@gmail.com>
2023-09-25 22:54:45 +0600

    fix responsive css

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -1,7 +1,14 @@
 import styles from "@/components/common/navbar/Navbar.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const handleResponsiveNav = () => {
+    setShowNav(!showNav);
+    document.body.classList.toggle("hide");
+  };
   return (
     <div className={styles["container"]}>
       <Link href="/" className={styles["logo"]}>
@@ -15,6 +22,27 @@ const Navbar = () => {
       <Link href="#footer" className={styles["contact-button"]}>
         Get In Touch
       </Link>
+      <img
+        src="/images/menu-icon.svg"
+        className={styles["responsive-nav"]}
+        onClick={handleResponsiveNav}
+      />
+      {showNav && (
+        <div
+          className={styles["container-responsive"]}
+          onClick={handleResponsiveNav}
+        >
+          <img
+            src="/images/close.svg"
+            className={styles["close"]}
+            onClick={handleResponsiveNav}
+          />
+          <Link href="#services">Services</Link>
+          <Link href="#works">Works</Link>
+          <Link href="#about">About</Link>
+          <Link href="#footer">Get In Touch</Link>
+        </div>
+      )}
     </div>
   );
 };
